8f5631a2a2e0dd6dbf03eec8ad065d3d88280334
Kevin Chen <xkevinchen@gmail.com>
2019-03-15 02:36:30 -0400

    Create TodoList components

---
+++ frontend/components/todo_list/todo_list.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import TodoListItem from './todo_list_item';
+
+class TodoList extends React.Component {
+  render() {
+    const { todos, receiveTodo } = this.props;
+    const todoItems = todos.map(todo => (
+        <TodoListItem
+          key={`todo-list-item${todo.id}`}
+          todo={todo}
+          receiveTodo={ receiveTodo } />
+      )
+    );
+
+    return (
+      <div>
+        <ul className="todo-list">
+          { todoItems }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Yay React!');
-  ReactDOM.render(<Root/>, document.getElementById('main'));
+  const preloadedState = localStorage.state ?
+  JSON.parse(localStorage.state) : {};
+  const store = configureStore(preloadedState);
+  
+  ReactDOM.render(<Root store={store} />, document.getElementById('main'));
 });
